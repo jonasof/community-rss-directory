60affd5a6d7f8ada1a507752405c0d5bcb60a0f8
Jonas Francisco <contato@jonasof.com.br>
2019-10-17 23:20:54 -0300

    Add coverage and laravel dusk to travis ci

--- app/Console/Commands/Dusk.php
+++ app/Console/Commands/Dusk.php
@@ -3,54 +3,50 @@
 namespace App\Console\Commands;
 
 use Symfony\Component\Process\Process;
-use Illuminate\Console\Command;
-use Artisan;
+use Laravel\Dusk\Console\DuskCommand;
 
-class Dusk extends Command
+class Dusk extends DuskCommand
 {
-    protected $signature = 'test:browser';
+    protected $signature = 'dusk {--without-tty : Disable output to TTY}';
 
-    protected $description = 'Run the Dusk tests for the application';
+    protected $description = 'Run the dusk tests with temporary sqlite database and embed server';
 
-    protected $process;
+    protected $databaseFile;
+    protected $databaseUri;
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        $this->prepareServer();
+        $this->createTemporaryDatabase();
 
-        $result = Artisan::call('dusk', [
-            '--env' => 'dusk'
-        ]);
-
-        $this->killServer();
-
-        return $result;
+        return $this->runTemporaryServer(function() {
+            return parent::handle();
+        });
     }
 
-    /**
-     * Build a process to run php artisan serve
-     *
-     * @return Process
-     */
-    protected function prepareServer()
+    protected function binary()
     {
-        echo shell_exec('echo "" > /tmp/dusk.sqlite');
+        return array_merge(["/usr/bin/env", "DB_DATABASE=$this->databaseUri"], parent::binary());
+    }
 
+    protected function createTemporaryDatabase()
+    {
+        $this->databaseFile = tmpfile();
+        $this->databaseUri = stream_get_meta_data($this->databaseFile)['uri'];
+    }
+
+    protected function runTemporaryServer(callable $callback)
+    {
         chdir(public_path());
 
-        $this->process = new Process('APP_ENV=dusk php -S 0.0.0.0:8001 ../server.php');
-        $this->process->start();
+        $server = new Process("DB_DATABASE=$this->databaseUri php -S 0.0.0.0:8001 ../server.php");
+        $server->start();
 
         chdir(base_path());
-    }
 
-    protected  function killServer()
-    {
-        $this->process->stop();
+        $return = $callback();
+
+        $server->stop();
+
+        return $return;
     }
 }
