54bc1b8e0f576b3f4ef10f49b1501c9f216741c6
Jonas Francisco <contato@jonasof.com.br>
2019-10-19 00:48:07 -0300

    Register dusk only if it's installed

--- app/Console/Commands/Dusk.php
+++ app/Console/Commands/Dusk.php
@@ -5,48 +5,51 @@ namespace App\Console\Commands;
 use Symfony\Component\Process\Process;
 use Laravel\Dusk\Console\DuskCommand;
 
-class Dusk extends DuskCommand
+if (class_exists('\Laravel\Dusk\Console\DuskCommand'))
 {
-    protected $signature = 'dusk {--without-tty : Disable output to TTY}';
-
-    protected $description = 'Run the dusk tests with temporary sqlite database and embed server';
-
-    protected $databaseFile;
-    protected $databaseUri;
-
-    public function handle()
+    class Dusk extends DuskCommand
     {
-        $this->createTemporaryDatabase();
+        protected $signature = 'dusk {--without-tty : Disable output to TTY}';
 
-        return $this->runTemporaryServer(function() {
-            return parent::handle();
-        });
-    }
+        protected $description = 'Run the dusk tests with temporary sqlite database and embed server';
 
-    protected function binary()
-    {
-        return array_merge(["/usr/bin/env", "DB_DATABASE=$this->databaseUri"], parent::binary());
-    }
+        protected $databaseFile;
+        protected $databaseUri;
 
-    protected function createTemporaryDatabase()
-    {
-        $this->databaseFile = tmpfile();
-        $this->databaseUri = stream_get_meta_data($this->databaseFile)['uri'];
-    }
+        public function handle()
+        {
+            $this->createTemporaryDatabase();
 
-    protected function runTemporaryServer(callable $callback)
-    {
-        chdir(public_path());
+            return $this->runTemporaryServer(function() {
+                return parent::handle();
+            });
+        }
 
-        $server = new Process("DB_DATABASE=$this->databaseUri php -S 0.0.0.0:8001 ../server.php");
-        $server->start();
+        protected function binary()
+        {
+            return array_merge(["/usr/bin/env", "DB_DATABASE=$this->databaseUri"], parent::binary());
+        }
 
-        chdir(base_path());
+        protected function createTemporaryDatabase()
+        {
+            $this->databaseFile = tmpfile();
+            $this->databaseUri = stream_get_meta_data($this->databaseFile)['uri'];
+        }
 
-        $return = $callback();
+        protected function runTemporaryServer(callable $callback)
+        {
+            chdir(public_path());
 
-        $server->stop();
+            $server = new Process("DB_DATABASE=$this->databaseUri php -S 0.0.0.0:8001 ../server.php");
+            $server->start();
 
-        return $return;
+            chdir(base_path());
+
+            $return = $callback();
+
+            $server->stop();
+
+            return $return;
+        }
     }
 }
